153d4a5bc5213ca57faa4d9b4b1c7a01e9358f20
lijianweideveloper <51866886+lijianweideveloper@users.noreply.github.com>
2021-01-07 16:35:26 +0800

    封装首页去后台拿数据的功能并在首页保存数据

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -6,11 +6,27 @@
 
 <script>
   import NavBar from "@/components/common/navbar/NavBar";
+  import {getHomeMultidata} from "../../network/home";
 
   export default {
     name: "Home",
     components: {
       NavBar
+    },
+    data() {
+      return {
+        result: null,
+        banners: [],
+        recommends: [],
+      }
+    },
+    created() {
+      getHomeMultidata().then(res => {
+        console.log(res);
+        this.result = res;
+        this.banners = res.data.banner.list;
+        this.recommends = res.data.recommend.list;
+      })
     },
   }
 </script>
